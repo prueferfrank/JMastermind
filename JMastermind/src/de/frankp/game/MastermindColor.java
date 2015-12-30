801323ab9c314a82e86c12baac44e4d6441d4308
Frank Prüfer <fpruefer@web.de>
2015-12-30 22:09:19 +0100

    initial

---
+++ JMastermind/src/de/frankp/game/MastermindColor.java
@@ -0,0 +1,12 @@
+package de.frankp.game;
+
+import java.awt.Color;
+
+public enum MastermindColor {
+RED(Color.RED);
+	private Color color;
+
+	private MastermindColor(Color color){
+		this.color = color;
+	}
+}
