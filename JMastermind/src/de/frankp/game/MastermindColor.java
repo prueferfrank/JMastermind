a6cd892dfb1cb319d5847d8ec3234e40228c7f9e
Frank Prüfer <fpruefer@web.de>
2015-12-30 22:13:26 +0100

    richtige Farben

--- JMastermind/src/de/frankp/game/MastermindColor.java
+++ JMastermind/src/de/frankp/game/MastermindColor.java
@@ -3,10 +3,16 @@ package de.frankp.game;
 import java.awt.Color;
 
 public enum MastermindColor {
-RED(Color.RED);
+	RED(Color.RED), //
+	GREEN(Color.GREEN), //
+	YELLOW(Color.YELLOW), //
+	BLUE(Color.BLUE), //
+	GRAY(Color.GRAY), //
+	WHITE(Color.WHITE);//
+
 	private Color color;
 
-	private MastermindColor(Color color){
+	private MastermindColor(Color color) {
 		this.color = color;
 	}
 }
